46712ea83dee7c30b243ff1fac97cc9d8a1dd659
Siôn Abraham <sion.abraham@senvo.de>
2025-03-30 15:25:35 +0200

    Update Next.js configuration to enable React strict mode and adjust image loader settings

--- blog/next.config.js
+++ blog/next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/sioncodes' : '',
   images: {
     loader: "akamai",
     path: "/",
     unoptimized: true
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
