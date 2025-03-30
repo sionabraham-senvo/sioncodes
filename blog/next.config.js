4f28be4d3fdccff389e36b0ea6ea1222188569ce
Siôn Abraham <sion.abraham@senvo.de>
2025-03-30 15:07:02 +0200

    Update Next.js configuration for blog directory and upgrade to version 15.2.4

--- blog/next.config.js
+++ blog/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/sioncodes' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/sioncodes/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/sioncodes/blog' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/sioncodes/blog/' : '',
   trailingSlash: true,
   images: {
     unoptimized: true, // GitHub Pages doesn't support Next.js Image Optimization
