5c3af134a9390de07e555f923c2ad737c420387d
Siôn Abraham <sion.abraham@senvo.de>
2025-04-09 13:57:16 +0200

    Update domain in Plausible script for accurate tracking

--- blog/src/app/layout.tsx
+++ blog/src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script defer data-domain="https://sioncodes.com" src="https://plausible.io/js/script.js"></script>
+        <script defer data-domain="sioncodes.com" src="https://plausible.io/js/script.js"></script>
         <link
           rel="apple-touch-icon"
           sizes="180x180"
